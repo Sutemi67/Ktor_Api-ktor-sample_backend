96d857f87d3811f59fba606aeddb816061a342fe
SergeyBoykov <sutemi67@gmail.com>
2025-05-13 06:44:36 +0300

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package apc.appcradle
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
